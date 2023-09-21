65324c4de4e9ce3c507e088ce8428430906f2b75
André Vasconcelos Souto <soutom@yahoo.com.br>
2023-09-20 23:13:03 -0300

    Corrigindo erros do H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into cliente(uuid, email, nome, data_nascimento) values ('12312-12311-123-123', 'andre@souto.com', 'andre', '1983-07-03')
+insert into cliente(id, uuid, email, nome, data_nascimento) values (1, '9e188af4-5822-11ee-8c99-0242ac120001', 'andre1@souto.com', 'andre1', '1983-07-01');
+insert into cliente(id, uuid, email, nome, data_nascimento) values (2, '9e188af4-5822-11ee-8c99-0242ac120002', 'andre2@souto.com', 'andre2', '1983-07-02');
+insert into cliente(id, uuid, email, nome, data_nascimento) values (3, '9e188af4-5822-11ee-8c99-0242ac120003', 'andre3@souto.com', 'andre3', '1983-07-03');
